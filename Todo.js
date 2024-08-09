e169b63960d89dd1517aa73f11dafb08b7d9ccfd
Nitin kumar <155936462+NitinRoshan18@users.noreply.github.com>
2024-08-09 11:41:04 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,24 @@
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = input.value;
+
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  delBtn.classList.add("delete");
+
+  item.appendChild(delBtn);
+  ul.appendChild(item);
+
+  input.value = "";
+});
+let delBtns = document.querySelectorAll(".delete");
+for (delBtn of delBtns) {
+  delBtn.addEventListener("click", function () {
+    let par = this.parentElement;
+    par.remove();
+  });
+}
